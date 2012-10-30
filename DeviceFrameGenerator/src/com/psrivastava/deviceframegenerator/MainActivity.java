88912ea398423c4d9ca14aaf25425446a7211b94
Prateek Srivastava <f2prateek@gmail.com>
2012-10-30 11:02:15 -0600

    Setup Skeleton
    -add ActionBarSherlock
    -add Crouton libs
    -setup app

---
+++ DeviceFrameGenerator/src/com/psrivastava/deviceframegenerator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.psrivastava.deviceframegenerator;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
