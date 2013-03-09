32aa1d1358918263aa2b0a90d9fa86a2fd704aa9
Prateek Srivastava <f2prateek@gmail.com>
2013-03-09 00:09:14 -0700

    Add some utils

---
+++ app/src/main/java/com/f2prateek/dfg/util/LogUtils.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Prateek Srivastava (@f2prateek)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.f2prateek.dfg.util;
+
+// LogUtil class, consistent naming
+// copied from https://code.google.com/p/iosched/source/browse/android/src/com/google/android/apps/iosched/util/LogUtils.java
+public class LogUtils {
+
+    private static final String LOG_PREFIX = "foodbot_";
+    private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
+    private static final int MAX_LOG_TAG_LENGTH = 23;
+    // usage example
+    private static final String LOGTAG = makeLogTag(LogUtils.class);
+
+    /**
+     * Make a LOGTAG for this class.
+     *
+     * @param cls
+     * @return
+     */
+    public static String makeLogTag(Class cls) {
+        return makeLogTag(cls.getSimpleName());
+    }
+
+    public static String makeLogTag(String str) {
+        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+            return LOG_PREFIX
+                    + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH
+                    - 1);
+        }
+
+        return LOG_PREFIX + str;
+    }
+
+}
---
+++ app/src/main/java/com/f2prateek/dfg/util/StorageUtils.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2013 Prateek Srivastava (@f2prateek)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.f2prateek.dfg.util;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+import android.util.Log;
+
+import static com.f2prateek.dfg.util.LogUtils.makeLogTag;
+
+public class StorageUtils {
+
+    /**
+     * Folder for storage finished images.
+     */
+    public static final String APP_FOLDER = "/Device-Frame-Generator/";
+    /**
+     * Storage directory, obtained by combining {@link #APP_FOLDER} and the
+     * environment path
+     */
+    public static final String STORAGE_DIRECTORY = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES).toString()
+            + APP_FOLDER;
+    private static final String LOGTAG = makeLogTag(StorageUtils.class);
+
+    /**
+     * Checks if storage is available for our use.
+     *
+     * @return true if storage is available and writeable
+     */
+    public static boolean checkStorageAvailable() {
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // We can read and write the media
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // We can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Something else is wrong. It may be one of many other states, but
+            // all we need to know is we can neither read nor write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }
+
+        Log.i(LOGTAG, mExternalStorageAvailable ? "Storage available" : "Storage Unavailable");
+        Log.i(LOGTAG, mExternalStorageWriteable ? "Storage writeable" : "Storage not writeable");
+
+        return (mExternalStorageAvailable && mExternalStorageWriteable);
+    }
+
+    /**
+     * Gets path of selected image
+     *
+     * @param context
+     * @param uri
+     * @return path of selected image
+     */
+    public static String getPath(Context context, Uri uri) {
+        String[] projection = {
+                MediaStore.Images.Media.DATA
+        };
+
+        CursorLoader cursorLoader = new CursorLoader(context, uri, projection, null, null, null);
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+}
