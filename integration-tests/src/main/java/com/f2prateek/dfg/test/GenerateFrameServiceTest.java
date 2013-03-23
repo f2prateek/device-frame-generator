ef184b9a61531b8a63d25a60a3d389b02d486ee2
Prateek Srivastava <f2prateek@gmail.com>
2013-03-23 03:32:13 -0600

    Travis config

--- integration-tests/src/main/java/com/f2prateek/dfg/test/GenerateFrameServiceTest.java
+++ integration-tests/src/main/java/com/f2prateek/dfg/test/GenerateFrameServiceTest.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.Suppress;
 import android.util.Log;
 import com.f2prateek.dfg.AppConstants;
 import com.f2prateek.dfg.core.DeviceFrameGenerator;
@@ -36,6 +37,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
+@Suppress
 public class GenerateFrameServiceTest extends ServiceTestCase<GenerateFrameService> {
 
     private static final int WAIT_TIME = 10;
