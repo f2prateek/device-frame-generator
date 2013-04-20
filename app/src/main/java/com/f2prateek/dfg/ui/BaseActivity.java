2fcf104ca3bcba06485725a2e2ad380f66dc1e1c
Prateek Srivastava <f2prateek@gmail.com>
2013-04-20 10:36:50 -0600

    Move copyright

--- app/src/main/java/com/f2prateek/dfg/ui/BaseActivity.java
+++ app/src/main/java/com/f2prateek/dfg/ui/BaseActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Prateek Srivastava (@f2prateek)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.f2prateek.dfg.ui;
 
 import android.content.Intent;
@@ -7,21 +23,6 @@ import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
 import com.f2prateek.dfg.DFGApplication;
 
-/**
- * Copyright 2013 prateek
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 public class BaseActivity extends SherlockFragmentActivity {
 
     @Override
