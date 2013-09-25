11cdc166b8726affbedcfc5f021a184e3632e566
Prateek Srivastava <f2prateek@gmail.com>
2013-09-25 14:59:12 -0600

    Just use 'this'

--- app/src/main/java/com/f2prateek/dfg/DFGApplication.java
+++ app/src/main/java/com/f2prateek/dfg/DFGApplication.java
@@ -41,7 +41,7 @@ public class DFGApplication extends Application {
     Picasso.with(this).setDebugging(BuildConfig.DEBUG);
     //noinspection PointlessBooleanExpression
     if (!BuildConfig.DEBUG) {
-      Crashlytics.start(getApplicationContext());
+      Crashlytics.start(this);
     }
 
     if (!StorageUtils.isStorageAvailable()) {
