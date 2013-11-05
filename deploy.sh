0077777d79082ce0fc22852dca5640aa9a751d9d
Prateek Srivastava <f2prateek@gmail.com>
2013-11-05 14:36:59 -0700

    More updates to building!

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# MANUAL : Bump version numbers in manifest
+# MANUAL : Bump version numbers in manifest, update build tools in build.gradle
+./gradlew clean check
 
 # Run tests
 ./test-cli.sh
 
 # MANUAL : Build a signed app
+./gradlew clean assembleRelease
 
 # Deploy website
 ./deploy_website.sh
