3da2de38b306a9e746916ded0bad303bd76bc002
Prateek Srivastava <f2prateek@gmail.com>
2013-12-20 10:02:22 -0700

    Make sure that some activity can handle this intent

--- app/src/main/java/com/f2prateek/dfg/ui/DeviceFragment.java
+++ app/src/main/java/com/f2prateek/dfg/ui/DeviceFragment.java
@@ -1,24 +1,27 @@
 /*
  * Copyright 2013 Prateek Srivastava (@f2prateek)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.f2prateek.dfg.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -37,6 +40,9 @@ import com.f2prateek.dfg.model.DeviceProvider;
 import com.f2prateek.dfg.util.BitmapUtils;
 import com.squareup.otto.Subscribe;
 import com.squareup.picasso.Picasso;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
+import java.util.List;
 import javax.inject.Inject;
 
 public class DeviceFragment extends BaseFragment {
@@ -120,8 +126,18 @@ public class DeviceFragment extends BaseFragment {
     Intent intent = new Intent();
     intent.setType("image/*");
     intent.setAction(Intent.ACTION_GET_CONTENT);
-    startActivityForResult(Intent.createChooser(intent, getString(R.string.select_picture)),
-        RESULT_SELECT_PICTURE);
+    if (isAvailable(activityContext, intent)) {
+      startActivityForResult(Intent.createChooser(intent, getString(R.string.select_picture)),
+          RESULT_SELECT_PICTURE);
+    } else {
+      Crouton.makeText(getActivity(), R.string.no_apps_available, Style.ALERT).show();
+    }
+  }
+
+  public static boolean isAvailable(Context ctx, Intent intent) {
+    final PackageManager mgr = ctx.getPackageManager();
+    List<ResolveInfo> list = mgr.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+    return list.size() > 0;
   }
 
   @OnClick(R.id.tv_device_name)
