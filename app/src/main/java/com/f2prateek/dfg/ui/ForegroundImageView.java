296dc71d37c9f877f94ec4e556b5bd2c925bedb2
Prateek Srivastava <f2prateek@gmail.com>
2014-03-23 23:57:10 -0600

    Better ForegroundImageView from u2020

--- app/src/main/java/com/f2prateek/dfg/ui/ForegroundImageView.java
+++ app/src/main/java/com/f2prateek/dfg/ui/ForegroundImageView.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2014 Prateek Srivastava (@f2prateek)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.f2prateek.dfg.ui;
@@ -19,68 +19,103 @@ package com.f2prateek.dfg.ui;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 import com.f2prateek.dfg.R;
 
 public class ForegroundImageView extends ImageView {
-
   private Drawable foreground;
-  private Rect foregroundBounds = new Rect();
 
   public ForegroundImageView(Context context) {
     this(context, null);
   }
 
   public ForegroundImageView(Context context, AttributeSet attrs) {
-    this(context, attrs, 0);
-  }
+    super(context, attrs);
 
-  public ForegroundImageView(Context context, AttributeSet attrs, int defStyle) {
-    super(context, attrs, defStyle);
-
-    TypedArray a =
-        context.obtainStyledAttributes(attrs, R.styleable.ForegroundImageView, defStyle, 0);
-    foreground = a.getDrawable(R.styleable.ForegroundImageView_android_foreground);
-    foreground.setCallback(this);
-
+    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ForegroundImageView);
+    Drawable foreground = a.getDrawable(R.styleable.ForegroundImageView_android_foreground);
+    if (foreground != null) {
+      setForeground(foreground);
+    }
     a.recycle();
   }
 
-  @Override
-  protected boolean verifyDrawable(Drawable who) {
+  /**
+   * Supply a drawable resource that is to be rendered on top of all of the child
+   * views in the frame layout.
+   *
+   * @param drawableResId The drawable resource to be drawn on top of the children.
+   */
+  public void setForegroundResource(int drawableResId) {
+    setForeground(getContext().getResources().getDrawable(drawableResId));
+  }
+
+  /**
+   * Supply a Drawable that is to be rendered on top of all of the child
+   * views in the frame layout.
+   *
+   * @param drawable The Drawable to be drawn on top of the children.
+   */
+  public void setForeground(Drawable drawable) {
+    if (foreground == drawable) {
+      return;
+    }
+    if (foreground != null) {
+      foreground.setCallback(null);
+      unscheduleDrawable(foreground);
+    }
+
+    foreground = drawable;
+
+    if (drawable != null) {
+      drawable.setCallback(this);
+      if (drawable.isStateful()) {
+        drawable.setState(getDrawableState());
+      }
+    }
+    requestLayout();
+    invalidate();
+  }
+
+  @Override protected boolean verifyDrawable(Drawable who) {
     return super.verifyDrawable(who) || (who == foreground);
   }
 
-  @Override
-  protected void drawableStateChanged() {
+  @Override public void jumpDrawablesToCurrentState() {
+    super.jumpDrawablesToCurrentState();
+    if (foreground != null) foreground.jumpToCurrentState();
+  }
+
+  @Override protected void drawableStateChanged() {
     super.drawableStateChanged();
+    if (foreground != null && foreground.isStateful()) {
+      foreground.setState(getDrawableState());
+    }
+  }
+
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     if (foreground != null) {
-      int[] drawableState = getDrawableState();
-      foreground.setState(drawableState);
+      foreground.setBounds(0, 0, getMeasuredWidth(), getMeasuredHeight());
+      invalidate();
     }
   }
 
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-    final Rect frameBounds = foregroundBounds;
-    if (frameBounds.isEmpty()) {
-      final int w = getWidth();
-      final int h = getHeight();
-
-      frameBounds.set(0, 0, w, h);
-      foreground.setBounds(frameBounds);
-    }
-
-    foreground.draw(canvas);
-  }
-
-  @Override
-  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+  @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
     super.onSizeChanged(w, h, oldw, oldh);
-    foregroundBounds.setEmpty();
+    if (foreground != null) {
+      foreground.setBounds(0, 0, w, h);
+      invalidate();
+    }
+  }
+
+  @Override public void draw(Canvas canvas) {
+    super.draw(canvas);
+
+    if (foreground != null) {
+      foreground.draw(canvas);
+    }
   }
 }
